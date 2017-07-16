4ff1c2b294845e42bcbd0df89e7459a2a8379cd4
Alex Webb <alexwebbb@protonmail.com>
2017-07-16 16:37:31 -0500

    hell yeah now we're talkin, got create react app w/ express working with no jsx. feeling like I better understand build process now

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,23 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import TitleComponent from './TitleComponent'
 
-class App extends Component {
-    // set display name, eslint wants this
-    displayName = "password app";
+var glorp = React.createElement;
 
-    // Initialize state
-    state = { passwords: [] };
 
-    // Fetch passwords after first mount
-    componentDidMount() {
-        this.getPasswords();
-    }
 
-    getPasswords = () => {
-        // Get the passwords and store them in state
-        fetch("/api/passwords")
-            .then(res => res.json())
-            .then(passwords => this.setState({ passwords }));
-    };
+// please include num1 and num2
+var AdditionComponent = (props) => {
+    return glorp('div', { className: 'math' },
+        props.num1 + props.num2
+    )
+}
 
-    render() {
-        const { passwords } = this.state;
+var App = () => {
+    return glorp('div', null,
+        glorp(TitleComponent, { title: 'Wuthering Heights', color: 'rebeccapurple' }),
+        glorp(AdditionComponent, { num1: 5, num2: 6 }),
 
-        return (
-            <div className="App">
-                {/* Render the passwords if we have them */}
-                {passwords.length
-                    ? <div>
-                        <h1>5 Passwords.</h1>
-                        <ul className="passwords">
-                            
-                            {/* Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array. */}
-              
-                            {passwords.map((password, index) =>
-                                <li key={index}>
-                                    {password}
-                                </li>
-                            )}
-                        </ul>
-                        <button className="more" onClick={this.getPasswords}>
-                            Get More
-              </button>
-                    </div>
-                    : // Render a helpful message otherwise
-                    <div>
-                        <h1>No passwords :(</h1>
-                        <button className="more" onClick={this.getPasswords}>
-                            Try Again?
-              </button>
-                    </div>}
-            </div>
-        );
-    }
+    );
 }
 
 export default App;
---
+++ client/src/TitleComponent.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+var glorp = React.createElement;
+
+
+// please include a title
+var TitleComponent = (props) => {
+    return glorp('div', null,
+        glorp('h1', { style: { color: props.color } }, props.title)
+    );
+}
+
+export default TitleComponent;
