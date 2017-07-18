816dd7d3f801405f8ab92eb6fed39ffdfd019ed8
Alex Webb <alexwebbb@protonmail.com>
2017-07-18 16:16:30 -0500

    switched to vid flix home page starter

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => (
+    <div className="app">
+        <div className="landing">
+            <h1>svideo</h1>
+            <input type="text" placeholder="Search" />
+            <a>or Browse All</a>
+        </div>
+    </div>
+);
+
+export default App;
