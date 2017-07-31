a7a276189ae2511dc8141855203e12d5ba5258b0
Alex Webb <alexwebbb@protonmail.com>
2017-07-31 01:13:11 -0500

    got images working! need to figure out a more efficient way to import many images

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -1,9 +1,27 @@
 ﻿import React from 'react';
 import preload from './data.json';
 
+
 const Search = () => (
     <div className="search">
-        <pre>{JSON.stringify(preload, null, 4)}</pre>
+        <div>
+            {preload.shows.map(show => (
+                <div className="show-card">
+                    {/* wow look at this hideous comment type
+                        at least it is multi line
+                        public url variable uses the "unsafe" public folder
+                        react hq would prefer you import images individually
+                        need to figure out how to import images as a bundle
+                    */}
+                    <img alt={`${show.title} Show Poster`} src={`./img/posters/${show.poster}`} />
+                    <div>
+                        <h3>{show.title}</h3>
+                        <h4>({show.year})</h4>
+                        <p>{show.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
 );
 
