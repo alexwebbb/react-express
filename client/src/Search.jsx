40dbb57697b2abf8a5edbcbc5473028da3ccab95
Alex Webb <alexwebbb@protonmail.com>
2017-08-06 01:56:00 -0500

    basic state

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -3,9 +3,32 @@ import ShowCard from './ShowCard'
 import preload from './data.json'
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchTerm: 'this is a debug statement'
+        };
+
+        this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    }
+
+    handleSearchTermChange(event) {
+        this.setState({ searchTerm: event.target.value })
+    }
+
     render() {
         return (
             <div className='search'>
+                <header>
+                    <h1>{this.state.searchTerm}</h1>
+                    <input
+                        onChange={this.handleSearchTermChange}
+                        value={this.state.searchTerm}
+                        type="text"
+                        placeholder="Search"
+                    />
+                </header>
                 <div>
                     {preload.shows.map((show) => {
                         return (
