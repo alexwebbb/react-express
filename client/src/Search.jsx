9eab03e7fb86d4abadb53a1498c8044d65e34eb7
Alex Webb <alexwebbb@protonmail.com>
2017-08-05 21:11:51 -0500

    props intro complete

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -1,28 +1,21 @@
-﻿import React from 'react';
-import preload from './data.json';
+﻿import React from 'react'
+import ShowCard from './ShowCard'
+import preload from './data.json'
 
-
-const Search = () => (
-    <div className="search">
-        <div>
-            {preload.shows.map(show => (
-                <div className="show-card">
-                    {/* wow look at this hideous comment type
-                        at least it is multi line
-                        public url variable uses the "unsafe" public folder
-                        react hq would prefer you import images individually
-                        need to figure out how to import images as a bundle
-                    */}
-                    <img alt={`${show.title} Show Poster`} src={`./img/posters/${show.poster}`} />
-                    <div>
-                        <h3>{show.title}</h3>
-                        <h4>({show.year})</h4>
-                        <p>{show.description}</p>
-                    </div>
+class Search extends React.Component {
+    render() {
+        return (
+            <div className='search'>
+                <div>
+                    {preload.shows.map((show) => {
+                        return (
+                            <ShowCard {...show} key={show.imdbID} />
+                        )
+                    })}
                 </div>
-            ))}
-        </div>
-    </div>
-);
+            </div>
+        )
+    }
+}
 
-export default Search;
+export default Search
---
+++ client/src/ShowCard.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { shape, string } from 'prop-types';
+
+const ShowCard = props => (
+    <div className="show-card">
+        <img alt={`${props.title} Show Poster`} src={`./img/posters/${props.poster}`} />
+        <div>
+            <h3>{props.title}</h3>
+            <h4>({props.year})</h4>
+            <p>{props.description}</p>
+        </div>
+    </div>
+);
+
+ShowCard.propTypes = {
+    show: shape({
+        poster: string.isRequired,
+        title: string.isRequired,
+        year: string.isRequired,
+        description: string.isRequired
+    }).isRequired
+};
+
+export default ShowCard;
